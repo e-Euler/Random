6b452fcaf18b7434c037039a18881c85f328d7e5
Jason Phillips <21094316+e-Euler@users.noreply.github.com>
2019-04-26 13:49:30 -0500

    Fixed error in Title tag being concatinated

--- exif_folder.sh
+++ exif_folder.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 for filename in ./*;
         do
-                printf "\rsetting title variable \t\t\t";
+                printf "\rsetting title variable                ";
                 printf "\r$filename\t\t\t";
-                title=$(exiftool -Title $filename| cut -d ":" -f 2 | tr -d '[:space:]');
-                printf "\rClearing metadata\t\t\t";
-                exiftool -q -m -all= -overwrite_original $filename;
-                printf "\rsetting title metadata\t\t\t";
-                exiftool -q -m -Title=$title $filename;
+                title=$(exiftool -Title "$filename" | cut -d ":" -f 2 | tr -d '[:space:]');
+                printf "\rClearing metadata                      ";
+                exiftool -q -m -overwrite_original -all= -overwrite_original "$filename";
+                printf "\rsetting title metadata                 ";
+                exiftool -q -m -overwrite_original -Title="$title" "$filename";
                 unset title;
         done
+
 
