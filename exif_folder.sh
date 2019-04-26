456801351398dda9f366be80f0de55478116400b
Jason Phillips <21094316+e-Euler@users.noreply.github.com>
2019-04-26 11:48:12 -0500

    added print suppression
    Attempting to fix title insertion. Currently files not containg a space receive correct title.

--- exif_folder.sh
+++ exif_folder.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-for filename in './*';
-do
-  title=$(exiftool -Title $filename| cut -d ":" -f 2 | tr -d '[:space:]');
-  exiftool -all= $filename;
-  exiftool -Title=$title $filename;
-  title=
-done
+for filename in ./*;
+        do
+                printf "\rsetting title variable \t\t\t";
+                printf "\r$filename\t\t\t";
+                title=$(exiftool -Title $filename| cut -d ":" -f 2 | tr -d '[:space:]');
+                printf "\rClearing metadata\t\t\t";
+                exiftool -q -m -all= -overwrite_original $filename;
+                printf "\rsetting title metadata\t\t\t";
+                exiftool -q -m -Title=$title $filename;
+                unset title;
+        done
 
